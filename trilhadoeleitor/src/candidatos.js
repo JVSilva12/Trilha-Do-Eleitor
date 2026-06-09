e8518a150120bf0a5438ac5f7ec73e27c97792e9
mateusrochabarbosa <mateusrochabarbosa.jobs@gmail.com>
2026-06-09 15:35:21 -0300

    Fotos de alguns candidatos (prefeito, governador, presidente e vereador)

--- trilhadoeleitor/src/candidatos.js
+++ trilhadoeleitor/src/candidatos.js
@@ -4,58 +4,58 @@
 
 // ---- PRESIDENTE ----
 export const CANDIDATOS_PRESIDENTE = {
-  '13': { nome: 'Lula da Silva',     partido: 'PT'       },
-  '22': { nome: 'Flávio Bolsonaro',   partido: 'PL'       },
-  '55': { nome: 'Ronaldo Caiado',    partido: 'PSD'      },
-  '14': { nome: 'Renan Santos',      partido: 'Missão'   },
-  '30': { nome: 'Romeu Zema',        partido: 'Novo'     },
-  '21': { nome: 'Edmilson Costa',    partido: 'PCB'      },
-  '27': { nome: 'Aldo Rabelo',       partido: 'DC'       },
-  '33': { nome: 'Cabo Daciolo',      partido: 'Mobiliza' },
-  '80': { nome: 'Samara Martins',    partido: 'UP'       },
-  '29': { nome: 'Rui Costa Pimenta', partido: 'PCO'      },
+  '13': { nome: 'Lula da Silva',     partido: 'PT',       foto: '/images/politicos/presidentes/LulaDaSilva.jpg'      },
+  '22': { nome: 'Flávio Bolsonaro',  partido: 'PL',       foto: '/images/politicos/presidentes/FlavioBolsonaro.jpg'  },
+  '55': { nome: 'Ronaldo Caiado',    partido: 'PSD',      foto: '/images/politicos/presidentes/RonaldoCaiado.jpg'    },
+  '14': { nome: 'Renan Santos',      partido: 'Missão',   foto: '/images/politicos/presidentes/RenanSantos.jpg'      },
+  '30': { nome: 'Romeu Zema',        partido: 'Novo',     foto: '/images/politicos/presidentes/RomeuZema.jpg'        },
+  '21': { nome: 'Edmilson Costa',    partido: 'PCB',      foto: '/images/politicos/presidentes/EdimilsonCosta.png'   },
+  '27': { nome: 'Aldo Rabelo',       partido: 'DC',       foto: '/images/politicos/presidentes/AldoRabelo.jpeg'      },
+  '33': { nome: 'Cabo Daciolo',      partido: 'Mobiliza', foto: '/images/politicos/presidentes/CaboDaciolo.jpg'      },
+  '80': { nome: 'Samara Martins',    partido: 'UP',       foto: '/images/politicos/presidentes/SamaraMartins.jpg'    },
+  '29': { nome: 'Rui Costa Pimenta', partido: 'PCO',      foto: '/images/politicos/presidentes/RuiCostaPimenta.jpg'  },
 };
 
 // ---- GOVERNADOR (Ceará) ----
 export const CANDIDATOS_GOVERNADOR = {
-  '45': { nome: 'Ciro Gomes',        partido: 'PSDB' },
-  '13': { nome: 'Elmano de Freitas', partido: 'PT'   },
-  '30': { nome: 'Eduardo Girão',     partido: 'Novo' },
-  '50': { nome: 'Jarir Pereira',     partido: 'PSOL' },
-  '16': { nome: 'Zé Batista',        partido: 'PSTU' },
+  '45': { nome: 'Ciro Gomes',        partido: 'PSDB', foto: '/images/politicos/governadores/CiroGomes.jpg'        },
+  '13': { nome: 'Elmano de Freitas', partido: 'PT',   foto: '/images/politicos/governadores/ElmanoDeFreitas.jpeg' },
+  '30': { nome: 'Eduardo Girão',     partido: 'Novo', foto: '/images/politicos/governadores/EduardoGirao.jpg'     },
+  '50': { nome: 'Jarir Pereira',     partido: 'PSOL', foto: '/images/politicos/governadores/JarirPereira.jpg'     },
+  '16': { nome: 'Zé Batista',        partido: 'PSTU', foto: '/images/politicos/governadores/ZeBatista.jpeg'       },
 };
 
 // ---- SENADOR ----
 export const CANDIDATOS_SENADOR = {
-  '130': { nome: 'Ana Paula Melo',      partido: 'PT'   },
-  '131': { nome: 'Carlos Bezerra Jr.', partido: 'PT'   },
-  '221': { nome: 'Felipe Martins',      partido: 'PL'   },
-  '222': { nome: 'Coronel Meira',       partido: 'PL'   },
-  '451': { nome: 'Tasso Jereissati',    partido: 'PSDB' },
-  '301': { nome: 'Alessandro Vieira',   partido: 'Novo' },
-  '551': { nome: 'Otto Alencar Filho',  partido: 'PSD'  },
-  '501': { nome: 'Fabiano Contarato',   partido: 'PT'   },
-  '121': { nome: 'Weverton Rocha',      partido: 'PDT'  },
+  '130': { nome: 'Ana Paula Melo',      partido: 'PT'    },
+  '131': { nome: 'Carlos Bezerra Jr.',  partido: 'PT'    },
+  '221': { nome: 'Felipe Martins',      partido: 'PL'    },
+  '222': { nome: 'Coronel Meira',       partido: 'PL'    },
+  '451': { nome: 'Tasso Jereissati',    partido: 'PSDB'  },
+  '301': { nome: 'Alessandro Vieira',   partido: 'Novo'  },
+  '551': { nome: 'Otto Alencar Filho',  partido: 'PSD'   },
+  '501': { nome: 'Fabiano Contarato',   partido: 'PT'    },
+  '121': { nome: 'Weverton Rocha',      partido: 'PDT'   },
   '141': { nome: 'Eduardo Bandeira',    partido: 'Missão'},
 };
 
 // ---- DEPUTADO FEDERAL ----
 export const CANDIDATOS_DEP_FEDERAL = {
-  '1301': { nome: 'José Guimarães',       partido: 'PT'   },
-  '1302': { nome: 'Luizianne Lins',       partido: 'PT'   },
-  '2201': { nome: 'Carmelo Neto',         partido: 'PL'   },
-  '2202': { nome: 'Dr. Jaziel',           partido: 'PL'   },
-  '4501': { nome: 'Roberto Pessoa',       partido: 'PSDB' },
-  '5001': { nome: 'Célia Xakriabá',       partido: 'PSOL' },
-  '5501': { nome: 'Domingos Neto',        partido: 'PSD'  },
-  '3001': { nome: 'Marcel van Hattem',    partido: 'Novo' },
-  '1201': { nome: 'André Figueiredo',     partido: 'PDT'  },
-  '4401': { nome: 'Danilo Forte',         partido: 'União'},
+  '1301': { nome: 'José Guimarães',       partido: 'PT'    },
+  '1302': { nome: 'Luizianne Lins',       partido: 'PT'    },
+  '2201': { nome: 'Carmelo Neto',         partido: 'PL'    },
+  '2202': { nome: 'Dr. Jaziel',           partido: 'PL'    },
+  '4501': { nome: 'Roberto Pessoa',       partido: 'PSDB'  },
+  '5001': { nome: 'Célia Xakriabá',       partido: 'PSOL'  },
+  '5501': { nome: 'Domingos Neto',        partido: 'PSD'   },
+  '3001': { nome: 'Marcel van Hattem',    partido: 'Novo'  },
+  '1201': { nome: 'André Figueiredo',     partido: 'PDT'   },
+  '4401': { nome: 'Danilo Forte',         partido: 'União' },
 };
 
 // ---- DEPUTADO ESTADUAL (Ceará) ----
 export const CANDIDATOS_DEP_ESTADUAL = {
-  '13001': { nome: 'Moisés Braz',          partido: 'PT'   },
+  '13001': { nome: 'Moisés Braz',           partido: 'PT'   },
   '13002': { nome: 'Renato Roseno',         partido: 'PT'   },
   '22001': { nome: 'Dra. Silvana',          partido: 'PL'   },
   '22002': { nome: 'Delegado Cavalcante',   partido: 'PL'   },
@@ -69,44 +69,44 @@ export const CANDIDATOS_DEP_ESTADUAL = {
 
 // ---- PREFEITO (Fortaleza) ----
 export const CANDIDATOS_PREFEITO = {
-  '22': { nome: 'André Fernandes',  partido: 'PL'            },
-  '13': { nome: 'Evandro Leitão',   partido: 'PT'            },
-  '12': { nome: 'José Sarto',       partido: 'PDT'           },
-  '44': { nome: 'Capitão Wagner',   partido: 'União'         },
-  '77': { nome: 'George Lima',      partido: 'Solidariedade' },
-  '50': { nome: 'Técio Nunes',      partido: 'PSol'          },
+  '22': { nome: 'André Fernandes',  partido: 'PL',            foto: '/images/politicos/prefeitos/AndreFernandes.jpg'  },
+  '13': { nome: 'Evandro Leitão',   partido: 'PT',            foto: '/images/politicos/prefeitos/EvandroLeitao.jpg'   },
+  '12': { nome: 'José Sarto',       partido: 'PDT',           foto: '/images/politicos/prefeitos/JoseSarto.jpg'       },
+  '44': { nome: 'Capitão Wagner',   partido: 'União',         foto: '/images/politicos/prefeitos/CapitaoWagner.jpg'   },
+  '77': { nome: 'George Lima',      partido: 'Solidariedade', foto: '/images/politicos/prefeitos/GeorgeLima.jpeg'     },
+  '50': { nome: 'Técio Nunes',      partido: 'PSol',          foto: '/images/politicos/prefeitos/TecioNunes.jpg'      },
 };
 
 // ---- VEREADOR (Fortaleza) ----
 export const CANDIDATOS_VEREADOR = {
-  '13001': { nome: 'Larissa Gaspar',       partido: 'PT'            },
-  '13002': { nome: 'Guilherme Sampaio',    partido: 'PT'            },
-  '22001': { nome: 'Priscila Costa',       partido: 'PL'            },
-  '22002': { nome: 'Leo Couto',            partido: 'PL'            },
-  '12001': { nome: 'Gardel Rolim',         partido: 'PDT'           },
-  '44001': { nome: 'Adail Júnior',         partido: 'União'         },
-  '50001': { nome: 'Enfermeira Milena',    partido: 'PSol'          },
-  '55001': { nome: 'Ronaldo Martins',      partido: 'PSD'           },
-  '77001': { nome: 'Luciano Girão',        partido: 'Solidariedade' },
-  '45001': { nome: 'Paula Pinheiro',       partido: 'PSDB'          },
+  '13133': { nome: 'Larissa Gaspar',       partido: 'PT',            foto: '/images/politicos/vereadores/LarissaGaspar.jpg'      },
+  '13234': { nome: 'Guilherme Sampaio',    partido: 'PT',            foto: '/images/politicos/vereadores/GuilhermeSampaio.jpeg'  },
+  '22222': { nome: 'Priscila Costa',       partido: 'PL',            foto: '/images/politicos/vereadores/PriscilaCosta.jpg'      },
+  '40123': { nome: 'Leo Couto',            partido: 'PSB',           foto: '/images/politicos/vereadores/LeoCouto.jpg'           },
+  '12001': { nome: 'Gardel Rolim',         partido: 'PDT',           foto: '/images/politicos/vereadores/GardelRolim.jpg'        },
+  '44001': { nome: 'Adail Júnior',         partido: 'União',         foto: '/images/politicos/vereadores/AdailJunior.jpeg'       },
+  '17741': { nome: 'Milena Cunha',         partido: 'PSL',           foto: '/images/politicos/vereadores/EnfermeiraMilena.jpg'   },
+  '10123': { nome: 'Ronaldo Martins',      partido: 'Republicanos',  foto: '/images/politicos/vereadores/RonaldoMartins.jpg'     },
+  '77001': { nome: 'Luciano Girão',        partido: 'Solidariedade', foto: '/images/politicos/vereadores/LucianoGirao.jpg'       },
+  '12921': { nome: 'Paula Pinheiro',       partido: 'PDT',          foto: '/images/politicos/vereadores/PaulaPinheiro.jpg'      },
 };
 
 // ---- 2º TURNO PRESIDENTE ----
 export const CANDIDATOS_PRESIDENTE_2T = {
-  '13': { nome: 'Lula da Silva',   partido: 'PT' },
-  '22': { nome: 'Flávio Bolsonaro', partido: 'PL' },
+  '13': { nome: 'Lula da Silva',    partido: 'PT', foto: '/images/politicos/presidentes/LulaDaSilva.jpg'     },
+  '22': { nome: 'Flávio Bolsonaro', partido: 'PL', foto: '/images/politicos/presidentes/FlavioBolsonaro.jpg' },
 };
 
 // ---- 2º TURNO GOVERNADOR (Ceará) ----
 export const CANDIDATOS_GOVERNADOR_2T = {
-  '13': { nome: 'Elmano de Freitas', partido: 'PT'   },
-  '45': { nome: 'Ciro Gomes',        partido: 'PSDB' },
+  '13': { nome: 'Elmano de Freitas', partido: 'PT',   foto: '/images/politicos/governadores/ElmanoDeFreitas.jpeg' },
+  '45': { nome: 'Ciro Gomes',        partido: 'PSDB', foto: '/images/politicos/governadores/CiroGomes.jpg'        },
 };
 
 // ---- 2º TURNO PREFEITO (Fortaleza) ----
 export const CANDIDATOS_PREFEITO_2T = {
-  '13': { nome: 'Evandro Leitão',  partido: 'PT' },
-  '22': { nome: 'André Fernandes', partido: 'PL' },
+  '13': { nome: 'Evandro Leitão',  partido: 'PT', foto: '/images/politicos/prefeitos/EvandroLeitao.jpg'  },
+  '22': { nome: 'André Fernandes', partido: 'PL', foto: '/images/politicos/prefeitos/AndreFernandes.jpg' },
 };
 
 // =========================================================================
